0a86c50b5b01169c7de282a200ea6b1ef9b7247c
Sid <sidchou93@gmail.com>
2019-03-05 11:51:47 -0500

    Initial Commit

---
+++ funController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  funController
+//
+//  Created by ching Hsi chou on 3/5/19.
+//  Copyright © 2019 ching Hsi chou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
